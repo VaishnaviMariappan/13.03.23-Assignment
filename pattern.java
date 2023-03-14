9bd38232294c193a5e8b944484bd62dade15a801
Vaishnavi M <94169913+VaishnaviMariappan@users.noreply.github.com>
2023-03-14 09:08:39 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,13 @@
+public class pattern
+{
+    public static void main(String[] args) {
+        int row = 5, i, j;
+        for (i = 1; i <=row; i++) {
+            for (j = 1; j <= row; j++)
+                System.out.print("* ");
+            System.out.print("\n");
+        }
+    }
+}
+
+
---
+++ pattern2.java
@@ -0,0 +1,18 @@
+public class pattern2
+{
+    public static void main(String[] args)
+    {
+        for(int i=5;i>=0;i--)
+        {
+            for(int j=0;j<=5-i;j++)
+            {
+                System.out.print("  ");
+            }
+            for(int k=1;k<=(2*i-1);k++)
+            {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
